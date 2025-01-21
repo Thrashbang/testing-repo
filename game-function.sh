6a9913638259e98ef62f21a708b0045e1ab885cc
Thrashbang <159561923+Thrashbang@users.noreply.github.com>
2025-01-21 13:51:24 -0700

    Create game-function.sh

---
+++ game-function.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e `cat gameScreen1`
+
+sleep 1
+
+echo -e `cat gameScreen2`
+
+sleep 1
+
+echo -e `cat gameScreen3`
+
+sleep 1
+
+echo -e `cat gameScreen4`
+
+sleep 1
+
+echo -e `cat gameScreen5`
+
+sleep 1
+
+echo -e `cat gameScreen6`
+
+sleep 1
+
+echo -e `cat gameScreen7`
+
+sleep 1
+
+exit
